e97f20da101b47edf8de7d5f577fe2480120d246
Anton Kholomiov <a.kholomyev@tinkoff.ru>
2023-08-06 11:05:54 +0300

    Remove commented code

--- src/Api.hs
+++ src/Api.hs
@@ -2,8 +2,7 @@
 -- For simplicity it is in single module, but will be split on parts
 -- by methods in real project.
 module Api
-  (
-   -- Api
+  ( -- Api
     SaveRequest(..)
   , SaveResponse(..)
   ) where
@@ -21,52 +20,3 @@ data SaveRequest = SaveRequest
 
 newtype SaveResponse = SaveResponse MessageId
   deriving newtype (ToJSON, FromJSON, Show, Eq)
-
-
-{-
-import Deriving.Aeson
-import Deriving.Aeson.Stock
-import Servant.API
-import Types
-
-type Api = "api" :> "v1" :>
-  (    SaveMessage
-  :<|> GetById
-  :<|> GetByTag
-  :<|> ToggleLog
-  )
-
-type SaveMessage =
-  "save" :> ReqBody '[JSON] SaveRequest :> Post '[JSON] SaveResponse
-
---------------------------------------------------------------------------
--- save
-
-data SaveRequest = SaveRequest
-  { message :: Text
-  , tags    :: [Tag]
-  }
-  deriving stock (Show, Generic)
-  deriving (FromJSON, ToJSON) via Vanilla SaveRequest
-
-newtype SaveResponse = SaveResponse MessageId
-  deriving newtype (ToJSON, FromJSON, Show, Eq)
-
---------------------------------------------------------------------------
--- get by id
-
-type GetById =
-  "get" :> "message" :> Capture "id" MessageId :> Get '[JSON] Message
-
---------------------------------------------------------------------------
--- get by tag
-
-type GetByTag =
-  "list" :> "tag" :> Capture "tag" Tag :> Get '[JSON] [Message]
-
---------------------------------------------------------------------------
--- toggle log
-
-type ToggleLog =
-  "toggle-logs" :> Post '[JSON] ()
--}
